04f0bdd5d608da652a324269c017e233b3c8f9dd
Jungho <jungho1016@gmail.com>
2023-08-24 12:35:18 +0900

    ADD : toast, 장바구니 상품 결제, 삭제기능 추가

--- lib/view/cart/cart_view.dart
+++ lib/view/cart/cart_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:freeshare/service/cart_service.dart';
+import 'package:freeshare/service/theme_service.dart';
+import 'package:freeshare/theme/component/button/button.dart';
 import 'package:freeshare/theme/component/pop_button.dart';
+import 'package:freeshare/theme/component/toast/toast.dart';
+import 'package:freeshare/util/helper/intl_helper.dart';
 import 'package:freeshare/util/lang/generated/l10n.dart';
+import 'package:freeshare/view/cart/widget/cart_empty.dart';
 import 'package:provider/provider.dart';
-
+import 'widget/cart_bottom_sheet.dart';
+import 'widget/cart_checkout_dialog.dart';
+import 'widget/cart_delete_dialog.dart';
 import 'widget/cart_item_tile.dart';
 
 class CartView extends StatelessWidget {
@@ -17,31 +24,85 @@ class CartView extends StatelessWidget {
         title: Text(S.current.cart),
         leading: const PopButton(),
         titleSpacing: 0,
-      ),
-      body: ListView.builder(
-        itemCount: cartService.cartItemList.length,
-        itemBuilder: (context, index) {
-          final cartItem = cartService.cartItemList[index];
-          return CartItemTile(
-            cartItem: cartItem,
+        actions: [
+          Button(
             onPressed: () {
-              cartService.update(
-                index,
-                cartItem.copyWith(
-                  isSelected: !cartItem.isSelected,
-                ),
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CartDeleteDialog(
+                    onDeletePressed: () {
+                      cartService.delete(cartService.selectedCartItemList);
+                      Toast.show(S.current.deleteDialogSuccessToast);
+                    },
+                  );
+                },
               );
             },
-            onCountChanged: (count) {
-              cartService.update(
-                index,
-                cartItem.copyWith(
-                  count: count,
-                ),
+            text: S.current.delete,
+            type: ButtonType.flat,
+            color: context.color.secondary,
+            isInactive: cartService.selectedCartItemList.isEmpty,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: cartService.cartItemList.isEmpty
+                ? const CartEmpty()
+                : ListView.builder(
+                    itemCount: cartService.cartItemList.length,
+                    itemBuilder: (context, index) {
+                      final cartItem = cartService.cartItemList[index];
+                      return CartItemTile(
+                        cartItem: cartItem,
+                        onPressed: () {
+                          cartService.update(
+                            index,
+                            cartItem.copyWith(
+                              isSelected: !cartItem.isSelected,
+                            ),
+                          );
+                        },
+                        onCountChanged: (count) {
+                          cartService.update(
+                            index,
+                            cartItem.copyWith(
+                              count: count,
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+          ),
+          CartBottomSheet(
+            totalPrice: cartService.selectedCartItemList.isEmpty
+                ? '0'
+                : IntlHelper.currency(
+                    symbol: cartService
+                        .selectedCartItemList.first.product.priceUnit,
+                    number:
+                        cartService.selectedCartItemList.fold(0, (prev, curr) {
+                      return prev + curr.count * curr.product.price;
+                    }),
+                  ),
+            selectedCartItemList: cartService.selectedCartItemList,
+            onCheckoutPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CartCheckoutDialog(
+                    onCheckoutPressed: () {
+                      cartService.delete(cartService.selectedCartItemList);
+                    },
+                  );
+                },
               );
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
